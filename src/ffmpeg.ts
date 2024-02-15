da98e0f3253ae86c07e8a77a4928c8a734aee8de
Paulius Gečas <hi@paulekas.eu>
2024-02-16 01:05:12 +0200

    refactor: join path function

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -14,23 +14,27 @@ const sh = (command: string): Promise<{ stdout: string, stderr: string }> => {
   });
 };
 
+const joinPath = (...paths: string[]) => {
+  return path.join(__dirname, '../', ...paths);
+}
 
 export const generateGIF = async (gif: string, identifier: string): Promise<Buffer> => {
   try {
-    const textPath = path.join(__dirname, `../`, `_temp/texts/${identifier}.png`)
-    const gifPath = path.join(__dirname, `../`, `assets/${gif}.gif`)
+    const textPath = joinPath(`_temp/texts/${identifier}.png`);
+    const gifPath = joinPath(`assets/${gif}.gif`)
+
     await sh(
-      `ffmpeg -i "${textPath}" -i "${gifPath}" -filter_complex "[0:v]scale=700:-1:flags=fast_bilinear[v0]; [1:v]scale=700:-1:flags=fast_bilinear[v1]; [v0][v1]vstack=inputs=2[v]" -map "[v]" "${path.join(__dirname, `../`, `_temp/${identifier}.gif`)}"`
+      `ffmpeg -i "${textPath}" -i "${gifPath}" -filter_complex "[0:v]scale=700:-1:flags=fast_bilinear[v0]; [1:v]scale=700:-1:flags=fast_bilinear[v1]; [v0][v1]vstack=inputs=2[v]" -map "[v]" "${joinPath(`_temp/${identifier}.gif`)}"`
     );
     await sh(
-      `ffmpeg -i "${path.join(__dirname, `../`, `_temp/${identifier}.gif`)}" -vf "fps=15,scale=350:-1" -fs 7M -loop 0 "${path.join(__dirname, `../`, `_temp/${identifier}-optimized.gif`)}"`
+      `ffmpeg -i "${joinPath(`_temp/${identifier}.gif`)}" -vf "fps=15,scale=350:-1" -fs 7M -loop 0 "${joinPath(`_temp/${identifier}-optimized.gif`)}"`
     );
 
-    const image = await fs.readFile(`./_temp/${identifier}-optimized.gif`);
+    const image = await fs.readFile(joinPath(`_temp/${identifier}-optimized.gif`));
 
-    await fs.unlink(`./_temp/texts/${identifier}.png`)
-    await fs.unlink(`./_temp/${identifier}.gif`)
-    await fs.unlink(`./_temp/${identifier}-optimized.gif`)
+    await fs.unlink(joinPath(`_temp/texts/${identifier}.png`))
+    await fs.unlink(joinPath(`_temp/${identifier}.gif`))
+    await fs.unlink(joinPath(`_temp/${identifier}-optimized.gif`))
 
     return image;
   } catch (err) {
